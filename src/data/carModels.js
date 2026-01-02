77bc838b62d1fbcb5b2fb8e639bf0520dc437860
Mohammed MHARZI <mohammed.mharzi1@gmail.com>
2026-01-02 12:58:13 +0100

    add complete car models list for all brands

---
+++ src/data/carModels.js
@@ -0,0 +1,145 @@
+export const carBrands = [
+  {
+    brand: "BMW",
+    models: [
+      "Series 1",
+      "Series 2",
+      "Series 3",
+      "Series 4",
+      "Series 5",
+      "Series 7",
+      "X1",
+      "X2",
+      "X3",
+      "X4",
+      "X5",
+      "X6",
+      "X7",
+      "Z4",
+      "M2",
+      "M3",
+      "M4",
+      "M5",
+      "M8",
+      "i4",
+      "i5",
+      "i7",
+      "iX"
+    ]
+  },
+
+  {
+    brand: "Audi",
+    models: [
+      "A1",
+      "A3",
+      "A4",
+      "A5",
+      "A6",
+      "A7",
+      "A8",
+      "Q2",
+      "Q3",
+      "Q4 e-tron",
+      "Q5",
+      "Q7",
+      "Q8",
+      "TT",
+      "R8",
+      "S3",
+      "S4",
+      "S6",
+      "RS3",
+      "RS4",
+      "RS5",
+      "RS6",
+      "RS7",
+      "e-tron GT"
+    ]
+  },
+
+  {
+    brand: "Volkswagen",
+    models: [
+      "Polo",
+      "Golf",
+      "Golf GTI",
+      "Golf R",
+      "Passat",
+      "Arteon",
+      "T-Roc",
+      "T-Cross",
+      "Tiguan",
+      "Touareg",
+      "ID.3",
+      "ID.4",
+      "ID.5",
+      "ID.7"
+    ]
+  },
+
+  {
+    brand: "Mercedes-Benz",
+    models: [
+      "A-Class",
+      "B-Class",
+      "C-Class",
+      "E-Class",
+      "S-Class",
+      "CLA",
+      "CLS",
+      "GLA",
+      "GLB",
+      "GLC",
+      "GLE",
+      "GLS",
+      "G-Class",
+      "AMG A45",
+      "AMG C43",
+      "AMG C63",
+      "AMG E53",
+      "AMG GT",
+      "EQE",
+      "EQS"
+    ]
+  },
+
+  {
+    brand: "Skoda",
+    models: [
+      "Fabia",
+      "Scala",
+      "Octavia",
+      "Octavia RS",
+      "Superb",
+      "Kamiq",
+      "Karoq",
+      "Kodiaq",
+      "Enyaq iV"
+    ]
+  },
+
+  {
+    brand: "SEAT",
+    models: [
+      "Ibiza",
+      "Leon",
+      "Leon FR",
+      "Arona",
+      "Ateca",
+      "Tarraco"
+    ]
+  },
+
+  {
+    brand: "CUPRA",
+    models: [
+      "Formentor",
+      "Leon",
+      "Leon Sportstourer",
+      "Ateca",
+      "Born",
+      "Tavascan"
+    ]
+  }
+];
